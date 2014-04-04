0da755c01841068cd8a495a2d1057b6f1d4ae9c3
GustavoRPS <gustavorps@gustavorps.net>
2014-04-04 00:13:19 -0300

    Melhorias no código  - Implementado RegExp para obter em separado o número, ano e numero do cantrato  - Implementado RegExp para obter número e ano da licitação  - Implementado RegExp para obter situação e data junto com verficação de quando for "Não Definida"

--- src/scraping/app.js
+++ src/scraping/app.js
@@ -6,8 +6,9 @@ var request = require('request')
 
 request = request.defaults({ strictSSL: false });
 
-var idObras = [4380, 4040, 4024, 4021, 4008, 3982],
-	concurrency = 3;
+var idObras = [4040];
+//var idObras = [4380, 4040, 4024, 4021, 4008, 3982];
+var concurrency = 3;
 
 var obras = [];
 
@@ -32,6 +33,7 @@ function scrapingObraDetalhe(obra) {
         if (err) throw err;
         var $ = cheerio.load(body);
 
+	  	obra.url = url;
 		obra.orgao_publico = $('table.listagem:nth-child(2) tr:nth-child(1) td:nth-child(2)').text().trim();
 	  	obra.bem_publico = $('table.listagem:nth-child(2) tr:nth-child(2) td:nth-child(2)').text().trim();
 	  	obra.descricao = $('table.listagem:nth-child(2) tr:nth-child(3) td:nth-child(2)').text().trim();
@@ -39,7 +41,7 @@ function scrapingObraDetalhe(obra) {
 	  	obra.setor_beneficiado = $('table.listagem:nth-child(2) tr:nth-child(5) td:nth-child(2)').text().trim();
 	  	obra.tipo = $('table.listagem:nth-child(2) tr:nth-child(6) td:nth-child(2)').text().trim();
 	  	obra.tipo_servico = $('table.listagem:nth-child(2) tr:nth-child(7) td:nth-child(2)').text().trim();
-	  	
+		
 	  	if (!obra.endereco) {
 	  		obra.endereco = {};
 	  	}
@@ -49,31 +51,41 @@ function scrapingObraDetalhe(obra) {
 		obra.endereco.cep =  $('table.listagem:nth-child(2) tr:nth-child(11) td:nth-child(2)').text().trim();
 
 	  	obra.engenheiros = {};
+		obra.engenheiros.fiscalizacao = [];
 		obra.engenheiros.fiscalizacao = $('table.listagem:nth-child(4) tr:nth-child(1) td:nth-child(2)').text().trim();
 		obra.engenheiros.execucao = $('table.listagem:nth-child(4) tr:nth-child(2) td:nth-child(2)').text().trim();
 
 	  	obra.contrato = {};
-		//TODO: Separar numero, ano e numero do cantrato
-		obra.contrato.numero = $('#ctl00_ContentPlaceHolderConteudo_contratoLabel').text().trim();
-		obra.contrato.ano = $('#ctl00_ContentPlaceHolderConteudo_contratoLabel').text().trim();
-		obra.contrato.numero_obra = $('#ctl00_ContentPlaceHolderConteudo_contratoLabel').text().trim();
+		var contrato = $('#ctl00_ContentPlaceHolderConteudo_contratoLabel').text().trim();
+		var contratoRegExp = new RegExp('(\\d+).*?(\\d+).*?(\\d+)');
+		obra.contrato.numero = contratoRegExp.exec(contrato)[1];
+		obra.contrato.ano =  contratoRegExp.exec(contrato)[2];
+		obra.contrato.numero_obra = contratoRegExp.exec(contrato)[3];
 		obra.contrato.data_assinatura = $('#ctl00_ContentPlaceHolderConteudo_dataAssinaturaLabel').text().trim();
 		obra.contrato.regime_execucao = $('#ctl00_ContentPlaceHolderConteudo_regimeExecucaoLabel').text().trim();
 	  	//TODO: Separar modalidade, numero e ano da licitacao
+		var licitacao = $('#ctl00_ContentPlaceHolderConteudo_editalNumeroLabel').text().trim();
+		var licitacaoRegExp = new RegExp('(\\d+).*?((?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])');
 	  	obra.contrato.modalidade_licitacao = $('#ctl00_ContentPlaceHolderConteudo_modalidadeLicitacaoLabel').text().trim();
-	  	obra.contrato.numero_licitacao = $('#ctl00_ContentPlaceHolderConteudo_editalNumeroLabel').text().trim();
-	  	obra.contrato.ano_licitacao = $('#ctl00_ContentPlaceHolderConteudo_editalNumeroLabel').text().trim();
+	  	obra.contrato.numero_licitacao = licitacaoRegExp.exec(licitacao)[1];
+	  	obra.contrato.ano_licitacao = licitacaoRegExp.exec(licitacao)[2];
 	  	obra.contrato.empresa = {};
 		obra.contrato.empresa.razao_social = $('#ctl00_ContentPlaceHolderConteudo_empresaConstrutoraLabel').text().trim();
 		obra.contrato.empresa.cnpj = $('#ctl00_ContentPlaceHolderConteudo_cnpjLabel').text().trim();
 		  	
 	  	obra.execucao = {};
-		obra.execucao.forma = $('#contratosTable tr:nth-child(2) td:nth-child(2)').text().trim(),
+		obra.execucao.forma = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(2) td:nth-child(2)').text().trim();
 		//obra.execucao.indireta = {};
-		obra.execucao.situacao = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(3) td:nth-child(2)').text().trim();
-		//TODO: Separar situação da data
-		obra.execucao.data = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(3) td:nth-child(2)').text().trim();
-		//TODO: Separar data da situação
+		obra.execucao.situacao = 'Não Definida';
+		obra.execucao.data = null;
+		var situacao = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(3) td:nth-child(2)').text().trim();
+		console.log(situacao);
+		if (situacao != 'Situação não definida -  -') {
+			//TODO: Melhorar RegExp
+			var situacaoRegExp = new RegExp('((?:[a-zA-Z]+))\\s+.*?\\s+((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3})))(?![\\d])');
+			obra.execucao.situacao = situacaoRegExp.exec(situacao)[1];
+			obra.execucao.data = situacaoRegExp.exec(situacao)[2];
+		}
 		obra.execucao.prazo_inicial = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(4) td:nth-child(2)').text().trim();
 		obra.execucao.prazo_aditado = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(4) td:nth-child(4)').text().trim();
 		obra.execucao.prazo_total = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(4) td:nth-child(6)').text().trim();
@@ -84,7 +96,7 @@ function scrapingObraDetalhe(obra) {
 		obra.execucao.valor_total_material = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(6) td:nth-child(4)').text().trim();
 		obra.execucao.valor_total_maquina_equipamento = $('#dadosObrasExecucaoIndiretaTable tr:nth-child(6) td:nth-child(6)').text().trim();
 
-		//console.log(obra);
+		console.log(obra);
 	});
 }
 
@@ -106,7 +118,7 @@ function scrapingObraLocalizacao(obra) {
 			longitude: longitude
 		};
 
-		console.log(obra);
+		//console.log(obra);
     });
 }
 
